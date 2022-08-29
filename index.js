c5fddbc67d6319a55de6c3c1ef3a94ee5e3ad0b3
“Liam <okeefe.liam@yahoo.com>
2022-08-29 15:48:27 -0500

    commit tree ad03682474f6040b2e76b6d10306933326988b4e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    if(distance > 42) {
+        return distance - 42
+    } else {
+        return 42 - distance
+    }
+}
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264
+}
+function distanceTravelledInFeet(starting, ending) {
+    const result = (ending - starting) * 264
+    if(starting > ending) {
+        return result * -1
+    } else {
+        return result
+    }
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet (start, destination)
+    if (distance <= 400) {
+        return 0
+    }
+}
